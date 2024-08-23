b9e65f45e98d5c906d607ab7bfaad656c3bcb752
phatdatle373 <136218473+phatdatle373@users.noreply.github.com>
2024-08-23 13:11:39 +0700

    upload

---
+++ config.js
@@ -0,0 +1,21 @@
+//====Nhập Thông Tin Của Bạn Vào Đây==============
+
+//Link Ảnh Background
+ImgLink_Background = "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/mot-em-be-tho-dang-iu-ma-sao-hoi-buon-hiu-do-1-1.jpg"
+
+//Link Ảnh Avatar
+ImgLink_Avatar = "https://i.postimg.cc/W4s3hFCM/LAm.png"
+
+//Nickname
+Nickname_Profile = "Nguyễn Ngọc Hoàng Lam"
+
+//Tên Thật
+RealName_Profile = "Khoa Kinh Tế - Quản Trị"
+
+//Các Link Của Bạn
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/profile.php?id=100030676816283",
+    Discord_LinkProfile = "#",
+    TikTok_LinkProfile = "#",
+    Instagram_LinkProfile = "#"
+]
